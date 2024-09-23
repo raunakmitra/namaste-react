13cbb6ceaf1963da84657077d642487d93289a52
Mitra, Raunak <raunakmitra8@gmail.com>
2024-09-23 16:04:39 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-//returns object/react element/js object
-//can be applied to a small portion of your app not same in a framework(come with baggage), do problem, will replace contents in the root,
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 const heading = React.createElement('h1',{id : 'anyValOrkey', xx:'yy'},'Hello world in React!')
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
